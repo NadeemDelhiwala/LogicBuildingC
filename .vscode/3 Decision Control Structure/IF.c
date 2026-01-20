04c771d8dddb76d5e29bee5e52d2c82c434b3004
Nadeem Ibrahim Delhiwala <nadeem3889@gmail.com>
2026-01-20 14:05:41 +0530

    3 Decision Control Structure

---
+++ .vscode/3 Decision Control Structure/IF.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+  /* code */
+  int age;
+  printf("Welcome to ARTO. \nPlease enter your age : ");
+  scanf("%d", &age);
+  printf("You have entered: %d\n", age);
+  if (age >= 18)
+  {
+    printf("You are eligible for Driving License.");
+  }
+   // 0  and NULL are considered false
+   //any non zero value is considered true
+
+  return 0;
+}
