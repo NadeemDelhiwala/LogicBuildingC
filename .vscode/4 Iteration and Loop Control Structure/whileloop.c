5bacbf8ccada6cd0c7c13d0d1f71e9fc5c3e4b8c
Nadeem Ibrahim Delhiwala <nadeem3889@gmail.com>
2026-01-25 22:52:12 +0530

    4 Iteration and Loop Control Structure

---
+++ .vscode/4 Iteration and Loop Control Structure/whileloop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+  int number = 1;
+  printf("While Loop Example: Print numbers from 1 to 10\n");
+  // while loop.
+  while (number <= 10)
+  {
+
+    // body of while loop
+    printf("%d\n", number);
+    number++;
+  }
+  printf("Loop ended as number is now %d\n", number);
+  printf("Exited from while loop.\n");
+  printf("End of the Program.\n");
+  printf("Thank you!\n");
+  printf("Printing numbers in reverse order from 10 to 1\n");
+  number = 10;
+  while (number >= 1)
+  {
+    printf("%d\n", number);
+    number--;
+    /* code */
+  }
+  printf("Loop ended as number is now %d\n", number);
+  printf("Exited from while loop.\n");
+  printf("End of the Program.\n");
+  printf("Thank you!\n");
+  return 0;
+}
